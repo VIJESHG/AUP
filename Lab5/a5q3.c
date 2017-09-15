642d2f870db20e4e8acd40cddb9a78878ad2576f
Vijesh Ghandare <ghandarevb14.comp@coep.ac.in>
2017-09-15 23:05:46 +0530

    modified question 3

--- Lab5/a5q3.c
+++ Lab5/a5q3.c
@@ -1,26 +1,21 @@
-#include<stdio.h>
+#include <stdio.h>
 #include<stdlib.h>
-#include<unistd.h>
-#include<sys/wait.h>
 #include<sys/types.h>
-int main(int argc, char *argv[]){
-	int pid, sleep_time, num_of_proc, i;
-	sleep_time = atoi(argv[2]);
-	num_of_proc = atoi(argv[1]);
-	for(i = 0; i < num_of_proc; i++){
-		pid = fork();
-		if (pid == 0){
-			printf("child %d sleeping...\n",i);
-			sleep(sleep_time);
-			exit(0);
-		}
-		else if(pid > 0){
-			printf("pid  %d \n", pid);
-			waitpid(pid - 1, NULL, 0);
-		}else{
-			printf("Error in fork \n");
+#include<sys/wait.h>
+#include<unistd.h>
+int main(int argc, char **argv) {	
+	int i= 0, pid;
+	int n = atoi(argv[1]);
+	for(i = 0; i < n; i++) {
+		if (pid = fork() == 0) { 
+			printf("Child %d\n",i);
+			sleep(atoi(argv[2]));
+			printf("exit child %d\n",i);
+			return 0;
+			
 		}
 	}
-	wait(0);
-	printf("All processes exited. \n");
+	while(wait(NULL) > 0);
+	printf("All the children have completed the execution..\n");
+	return 0;
 }
