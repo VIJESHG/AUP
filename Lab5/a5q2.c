ec8f5ca9420dc25581ddcbf4d7c3e75713856c0a
Vijesh Ghandare <ghandarevb14.comp@coep.ac.in>
2017-09-16 08:57:59 +0530

    added comments

--- Lab5/a5q2.c
+++ Lab5/a5q2.c
@@ -9,24 +9,24 @@ int main(int argc, char **argv) {
 	int i= 0, pid,fd;
 	char *str;
 	str = (char*)malloc(sizeof(char)*128);
-		pid = fork();
+		pid = fork();							//fork
 		if (pid > 0) {
 			printf("Parent started..\n"); 
-			fd = open(argv[1],O_RDONLY);
+			fd = open(argv[1],O_RDONLY);		//file opened in parent
 			if(fd < 0)
 				printf("open failed...!!!\n");
 		}
 		else{
 			sleep(2);
 			printf("Child started..\n");
-			if(read(fd,str,128) < 0)
+			if(read(fd,str,128) < 0)			//reading from file opened by parent
 				printf("Read from file opened by parent failed in child...\n");
 			else
 				printf("%s",str);
 			printf("Child Exit..\n");
 			return 0;			
 		}
-		wait(NULL);
+		wait(NULL);								//wait until child exits.						
 		printf("Parent Exit..\n");
 	return 0;
 }
