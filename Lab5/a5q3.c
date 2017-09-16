0f963dcf75a3e44db7a29bb50fad589e945ce9e9
Vijesh Ghandare <ghandarevb14.comp@coep.ac.in>
2017-09-16 08:58:36 +0530

    added comments

--- Lab5/a5q3.c
+++ Lab5/a5q3.c
@@ -5,17 +5,17 @@
 #include<unistd.h>
 int main(int argc, char **argv) {	
 	int i= 0, pid;
-	int n = atoi(argv[1]);
+	int n = atoi(argv[1]);				//no. of processes
 	for(i = 0; i < n; i++) {
-		if (pid = fork() == 0) { 
-			printf("Child %d\n",i);
-			sleep(atoi(argv[2]));
+		if (pid = fork() == 0) { 			//child
+			printf("Child %d\n",i);	
+			sleep(atoi(argv[2]));			//sleeping
 			printf("exit child %d\n",i);
 			return 0;
 			
 		}
 	}
-	while(wait(NULL) > 0);
+	while(wait(NULL) > 0);					//wait until all child exits.
 	printf("All the children have completed the execution..\n");
 	return 0;
 }
