3a01fdc279b8f79a3e7b60d281237db5dfe6b33f
Vijesh Ghandare <ghandarevb14.comp@coep.ac.in>
2017-10-02 00:26:27 +0530

    done final modification

--- Lab6/a6q1.c
+++ Lab6/a6q1.c
@@ -2,17 +2,13 @@
 #include <pthread.h>
 #include <string.h>
 #include <stdio.h>
-#include <semaphore.h>
-
-int file_index = 0;         // index for array[500];
-
-struct params {
+int file_index = 0;        
+struct params {									/*Parameters struct*/
     char file_names [255];
     char word[255];           
 };
-
-void *thread(void *wData_element){
-    struct params *temp = wData_element;
+void *search(void *args){						/*thread function*/						
+    struct params *temp = args;
 
     FILE *fp; 
     char line[255]="";      
@@ -22,28 +18,28 @@ void *thread(void *wData_element){
         perror("Error: File open failure.");
     }
     else{
-        fgets(line,255, fp);
-        char *token = strtok(line, " \t");
-    	while (token != NULL){
-        	//printf("%s\n", token);
-			if (strstr(token,temp->word) != NULL){
-        		printf("%s", strstr(token,temp->word));
-        		fclose(fp);
-        		return NULL;	
-        	}        	
-        	token = strtok(NULL, " \t");
-    	}
+        while(fgets(line,255, fp)){				/*read and tokenize each line and search using strstr() */
+		    char *token = strtok(line, " \t");
+			while (token != NULL){
+				if (strstr(token,temp->word) != NULL){
+		    		printf("Pattern found:[ %s ] in file %s \n", strstr(token,temp->word), temp->file_names);
+		    		fclose(fp);
+		    		return (void*) 1;	
+		    	}        	
+		    	token = strtok(NULL, " \t");
+			}
+		}
     }
     fclose(fp);
-    return NULL;
+    return (void*) 0;
 }
 
 int main(int argc, char const* argv[]) {
+	void *result;
 	char dir_path[255];
 	char word[255];
-    DIR * dir_pointer;          // define a dir pointer;
-    struct dirent * entry;      // entry under dir;
-     // data directory contains all files in which we want to search.
+    DIR * dir_pointer;          
+    struct dirent * entry;      
     int count;
 	printf("Enter the path of dir containing all files:");
 	scanf("%s",dir_path);
@@ -51,31 +47,30 @@ int main(int argc, char const* argv[]) {
 	scanf("%d",&count);
 	printf("Enter word to be searched:");
 	scanf("%s",word); 
-    struct params wData[count];
-    pthread_t tid_array[count];
+    struct params pars[count];
+    pthread_t threads[count];
 	
     if ((dir_pointer = opendir(dir_path)) == NULL) {
     	printf("can't open directory\n");
     	return 0;
   	}
     while( (entry = readdir(dir_pointer)) != NULL){
-
-        if(entry->d_type == DT_REG){ // avoid the . and .. dir;
+        if(entry->d_type == DT_REG){ 						/*access only regular files i.e avoid the . and .. dir */
             char full_path[255];     
-            full_path[0] = '\0';    // initilize the string;
-
-            strcat(full_path, dir_path);  // concatenate file directory;
-            strcat(full_path, entry->d_name);    // concatenate filename;
-            strcpy(wData[file_index].file_names, full_path); // store file name into file_names array;
-            strcpy(wData[file_index].word,word);
-            pthread_create(&tid_array[file_index], NULL, thread, &wData[file_index]);
-
-            file_index++;   // increase the file index for next file.
+            full_path[0] = '\0';   
+            strcat(full_path, dir_path);  					// concatenate file directory;
+            strcat(full_path, entry->d_name);    			// concatenate filename;
+            strcpy(pars[file_index].file_names, full_path); // store file name into file_names array;
+            strcpy(pars[file_index].word,word);
+            pthread_create(&threads[file_index], NULL, search, &pars[file_index]);  //create thread for each file
+            file_index++;   					
         }
     }
-
-    for(int i=0; i < count; i++){
-        pthread_join(tid_array[i], NULL);
+    for(int i = 0; i < count; i++){
+        pthread_join(threads[i],&result);
+        if((int*)result)
+ 			return 0;
     }
+    printf("Word not found..!!!\n");
     return 0;
 }
